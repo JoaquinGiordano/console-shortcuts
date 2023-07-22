39129f24c3f8e21779b13bac80bcf3502f780bea
JoaquinGiordano <joaquin_giordano@outlook.com>
2023-07-22 19:42:58 -0300

    Solving headers location bug

--- headers/program.h
+++ headers/program.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "headers/constants.h"
-#include "headers/storage.h"
-#include "headers/commands.h"
+#include "constants.h"
+#include "storage.h"
+#include "commands.h"
 
 int main(int argc, char *argv[]);
 
