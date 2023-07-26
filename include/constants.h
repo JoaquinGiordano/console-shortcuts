474b5318d7b9f86beeec1c5c12b9925ba4a38e05
JoaquinGiordano <joaquin_giordano@outlook.com>
2023-07-26 00:20:08 -0300

    Changing the savepath again

--- include/constants.h
+++ include/constants.h
@@ -2,8 +2,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define SAVE_FOLDER "C:\\ProgramData\\console-shortcuts"
-#define SAVE_PATH "C:\\ProgramData\\console-shortcuts\\save.sf"
+#define SAVE_FOLDER "C:\\ProgramData\\ConsoleShortcuts"
+#define SAVE_PATH "C:\\ProgramData\\ConsoleShortcuts\\save.sf"
 #define MAX_COMMAND_LENGHT 50
 #define MAX_DIR_LENGHT 200
 #define DEFAULT_STORAGE_SIZE 10
